ea800d042e75ff4aed20bf55f9e5f11d70d3a5e6
SGuevaraClark <sebastianguevarac@gmail.com>
2024-10-07 15:58:02 +0000

    Second Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const who = ["The dog", "My grandma", "The mailman", "My bird"];
+const action = ["ate", "peed", "crushed", "broke"];
+const what = ["my homework", "my phone", "the car"];
+const when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const generateExcuse = (who, action, what, when) => {
+  const randomElement = arr => arr[Math.floor(Math.random() * arr.length)];
+  return `${randomElement(who)} ${randomElement(action)} ${randomElement(
+    what
+  )} ${randomElement(when)}.`;
 };
+
+const displayExcuse = () => {
+  const excuse = generateExcuse(who, action, what, when);
+  document.getElementById("excuse").innerHTML = excuse;
+};
+
+window.onload = displayExcuse;
